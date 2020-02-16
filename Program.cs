63b24144db782a5924f5b1ce238949eaf7b9e709
Evgenestan <61114102+Evgenestan@users.noreply.github.com>
2020-02-16 17:42:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libtest;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            figura f0 = new figura();
+            figura f1 = new figura(5);
+            figura f2 = new figura(4,6);
+            figura f3 = new figura(4,6,6);
+            f1.area();
+
+
+        }
+    }
+}
